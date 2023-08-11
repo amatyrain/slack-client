a6ddfb294e8f235c3d61a89be396ee70535261c5
amaty <nwhfd.2022@gmail.com>
2023-08-11 15:42:59 +0900

    initial

---
+++ slack.py
@@ -0,0 +1,14 @@
+import slackweb
+
+
+class Slack:
+    def send_slack_message(self, url, message):
+        slack = slackweb.Slack(url=url)
+        slackbot_icon_emoji = ":ghost:"
+        slackbot_username = 'webhookbot'
+
+        slack.notify(
+            text=message,
+            icon_emoji=slackbot_icon_emoji,
+            username=slackbot_username,
+        )
